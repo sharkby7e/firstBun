ed196a3c01c4bbc0d59415b48d068fc3f886188f
sharkbyte <sgquin@gmail.com>
2022-07-06 22:41:51 -0700

    removed base react app, added the chessboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import logo from "./logo.svg";
 import * as React from "react";
 import "./App.css";
+import Board from "./components/chessboard.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Board />;
 }
 
 export default App;
---
+++ src/components/chessboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Chessboard } from "react-chessboard";
+
+function Board() {
+  return (
+    <div>
+      <Chessboard id="BasicBoard" />
+    </div>
+  );
+}
+
+export default Board;
